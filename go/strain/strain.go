833ac168779735513e9ef0bdf61842bf54499471
fenogentov <ShuraF@andex.ru>
2021-05-21 17:41:37 +0300

    init "strain"

--- go/strain/strain.go
+++ go/strain/strain.go
@@ -1,1 +1,19 @@
 package strain
+
+type Ints []int
+type Lists [][]int
+type Strings []string
+
+func (Ints) Keep(func(int) bool) Ints {
+	return nil
+}
+
+func (Ints) Discard(func(int) bool) Ints {
+	return nil
+}
+func (Lists) Keep(func([]int) bool) Lists {
+	return nil
+}
+func (Strings) Keep(func(string) bool) Strings {
+	return nil
+}
