5d91e8cf83cc5a9008c3c609d03ea46c2849a7fb
fenogentov <ShuraF@andex.ru>
2021-05-25 15:24:56 +0300

    resolved

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,1 +1,19 @@
+// Package diffsquares is a solution to lerning #7 (exercism.io)
 package diffsquares
+
+// SquareOfSum calculates the Square of the sum of n natural numbers
+func SquareOfSum(n int) int {
+	sum := (1 + n) * n / 2
+	return sum * sum
+}
+
+//SumOfSquares calculates sum of the squares of the first n natural numbers is
+func SumOfSquares(n int) int {
+	squar := n * (n + 1) * (2*n + 1) / 6
+	return squar
+}
+
+// Difference  between the square of the sum of the first n natural numbers and the sum of the squares of the first n natural numbers
+func Difference(n int) int {
+	return (SquareOfSum(n) - SumOfSquares(n))
+}
