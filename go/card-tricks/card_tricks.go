df287fe859341119f7b08ca9795f0b3ac80d9004
fenogentov <ShuraF@andex.ru>
2021-09-08 09:10:49 +0300

    card-tricks is completed

---
+++ go/card-tricks/card_tricks.go
@@ -0,0 +1,46 @@
+// Package cards manipulates cards
+package cards
+
+import "fmt"
+
+// GetItem retrieves an item from a slice at given position. The second return value indicates whether
+// a the given index existed in the slice or not.
+func GetItem(slice []uint8, index int) (uint8, bool) {
+	if index < 0 || index > (len(slice)-1) {
+		return 0, false
+	}
+	s := slice[index]
+	fmt.Println(s)
+	return s, true
+}
+
+// SetItem writes an item to a slice at given position overwriting an existing value.
+// If the index is out of range it is be appended.
+func SetItem(slice []uint8, index int, value uint8) []uint8 {
+	if index < 0 || index > (len(slice)-1) {
+		slice = append(slice, value)
+	} else {
+		slice[index] = value
+	}
+	return slice
+}
+
+// PrefilledSlice creates a slice of given length and prefills it with the given value.
+func PrefilledSlice(value, length int) []int {
+	if length <= 0 {
+		return nil
+	}
+	slice := make([]int, length)
+	for i := 0; i < length; i++ {
+		slice[i] = value
+	}
+	return slice
+}
+
+// RemoveItem removes an item from a slice by modifying the existing slice.
+func RemoveItem(slice []int, index int) []int {
+	if index < 0 || index > (len(slice)-1) {
+		return slice
+	}
+	return append(slice[:index], slice[index+1:]...)
+}
