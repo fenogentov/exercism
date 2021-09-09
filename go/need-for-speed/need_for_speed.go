625f03abc1167d0a1cadc022868cec0b4e4802ef
fenogentov <ShuraF@andex.ru>
2021-09-09 15:16:59 +0300

    need-for-speed is completed

---
+++ go/need-for-speed/need_for_speed.go
@@ -0,0 +1,48 @@
+package speed
+
+// Car implements a remote controlled car.
+type Car struct {
+	speed        int
+	batteryDrain int
+	battery      int
+	distance     int
+}
+
+// NewCar creates a new remote controlled car with full battery and given specifications.
+func NewCar(speed, batteryDrain int) Car {
+	return Car{
+		speed:        speed,
+		batteryDrain: batteryDrain,
+		battery:      100,
+	}
+}
+
+// Track implements a race track.
+type Track struct {
+	distance int
+}
+
+// NewTrack created a new track
+func NewTrack(distance int) Track {
+	return Track{distance: distance}
+}
+
+// Drive drives the car one time. If there is not enough battry to drive on more time,
+// the car will not move but use the leftover battery.
+func Drive(car Car) Car {
+	if car.batteryDrain > car.battery {
+		return car
+	}
+	return Car{
+		speed:        car.speed,
+		batteryDrain: car.batteryDrain,
+		battery:      car.battery - car.batteryDrain,
+		distance:     car.distance + car.speed,
+	}
+}
+
+// CanFinish checks if a car is able to finish a certain track.
+func CanFinish(car Car, track Track) bool {
+	batteryNeeded := track.distance * car.batteryDrain / car.speed
+	return batteryNeeded <= car.battery
+}
