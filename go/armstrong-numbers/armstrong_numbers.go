df74b571f2e391e9926fdddbb6dfd99dd6eee429
fenogentov <ShuraF@andex.ru>
2021-09-21 17:41:14 +0300

    armstrong-numbers is optimized

--- go/armstrong-numbers/armstrong_numbers.go
+++ go/armstrong-numbers/armstrong_numbers.go
@@ -10,10 +10,30 @@ func IsNumber(N int) bool {
 		return true
 	}
 	var summ int
+	grade := int(math.Log10(float64(N))) + 1
+	for i := 10; i < N*10; i *= 10 {
+		d := N % i / (i / 10)
+		summ += intPow(d, grade)
+	}
+	return summ == N
+}
+func IsNumberV1(N int) bool {
+	if N < 10 {
+		return true
+	}
+	var summ int
 	grade := len(strconv.Itoa(N))
 	for i := 10; i < N*10; i *= 10 {
 		a := N % i / (i / 10)
 		summ += int(math.Pow(float64(a), float64(grade)))
 	}
 	return summ == N
+}
+
+func intPow(n, grade int) (pow int) {
+	pow = n
+	for i := 1; i < grade; i++ {
+		pow *= n
+	}
+	return pow
 }
--- go/armstrong-numbers/armstrong_numbers_test.go
+++ go/armstrong-numbers/armstrong_numbers_test.go
@@ -26,3 +26,16 @@ func BenchmarkIsNumber(b *testing.B) {
 		})
 	}
 }
+
+func BenchmarkIsNumberV1(b *testing.B) {
+	if testing.Short() {
+		b.Skip("skipping benchmark in short mode.")
+	}
+	for _, tc := range testCases {
+		b.Run(fmt.Sprintf("%d", tc.input), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				IsNumberV1(tc.input)
+			}
+		})
+	}
+}
