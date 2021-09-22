d3a6c052485bf28bf5ebf0dcde0961287341bc3c
fenogentov <ShuraF@andex.ru>
2021-09-22 08:55:11 +0300

    all-your-base is completed

--- go/all-your-base/all_your_base.go
+++ go/all-your-base/all_your_base.go
@@ -1,5 +1,67 @@
+// Package allyourbase implements tools for converting a sequence of numbers
+// from one base to any other base.
 package allyourbase
 
-func ConvertToBase(inBase int, digits []int, outBase int) ([]int, error) {
+import (
+	"errors"
+)
+
+var (
+	errInBase  = errors.New("input base must be >= 2")
+	errOutBase = errors.New("output base must be >= 2")
+	errDigit   = errors.New("all digits must satisfy 0 <= d < input base")
+)
+
+// ConvertToBase converts a sequence of numbers with base 'inBase'
+// to a sequence of numbers with base 'outBase'
+func ConvertToBase(inBase int, digits []int, outBase int) (out []int, err error) {
+	if inBase < 2 {
+		return nil, errInBase
+	}
+	if outBase < 2 {
+		return nil, errOutBase
+	}
+	N, err := toTenBase(digits, inBase)
+	if err != nil {
+		return nil, err
+	}
+	out = toAnyBase(N, outBase)
+	return out, nil
+}
+
+// intPow performs exponentiation for integers.
+func intPow(digit, grade int) (res int) {
+	if grade == 0 {
+		return 1
+	}
+	if grade == 1 {
+		return digit
+	}
+	res = digit
+	for i := 1; i < grade; i++ {
+		res *= digit
+	}
+	return res
+}
+
+// toTenBase converts a sequence of numbers with an arbitrary base gets a number with base 10.
+func toTenBase(digits []int, inBase int) (N int, err error) {
+	for i := 0; i < len(digits); i++ {
+		if digits[i] < 0 || digits[i] > (inBase-1) {
+			return 0, errDigit
+		}
+		N += digits[i] * intPow(inBase, (len(digits)-i-1))
+	}
+	return N, nil
+}
+
+// toAnyBase gets from base 10 a sequence of numbers with any base
+func toAnyBase(digit, outBase int) (out []int) {
+	if digit < outBase {
+		out = append(out, digit)
+		return
+	}
+	r := toAnyBase(digit/outBase, outBase)
+	return append(r, digit%outBase)
 
 }
--- go/simple-cipher/cipher.go
+++ go/simple-cipher/cipher.go
@@ -1,20 +1,117 @@
 package cipher
 
+import (
+	"strings"
+	"unicode"
+)
+
 type Cipher interface {
 	Encode(string) string
 	Decode(string) string
 }
 
-func (*Cipher) Encode(string) string {
+func (c *Cipher) Encode(str string) string {
+	f := func(r rune) rune {
+		if !unicode.IsLetter(r) {
+			return -1
+		}
+		return r
+	}
+	strings.Map(f, str)
 	return ""
 }
 
-func (*Cipher) Decode(string) string {
+func (c *Cipher) Decode(string) string {
 
 }
 
-func NewCaesar() Cipher {}
+func NewCaesar() Cipher {
+	return NewShift(3)
+}
 
-func NewShift(in int) Cipher {}
+func NewShift(distance int) Cipher {
+	if distance < -25 || distance > 25 || distance == 0 {
+		return nil
+	}
+}
 
-func NewVigenere(str string) Cipher {}
+func NewVigenere(key string) Cipher {
+
+	for _, r := range key {
+		if !unicode.IsLower(r) {
+			return nil
+		}
+	}
+	if strings.Count(key, "a") == len(key) {
+		return nil
+	}
+
+}
+
+// var rng = byte('z' - 'a' + 1)
+// // NewCaesar returns a Caesar cipher, based on Shift cipher
+// func NewCaesar() Cipher {
+// 	return NewShift(3)
+// }
+// // NewShift returns a Shift cipher, based on Vigenere cipher
+
+// func NewShift(k int) Cipher {
+// 	if k == 0 || k > 25 || k < -25 {
+// 		return nil
+// 	}
+// 	n := byte(k)
+// 	return NewVigenere(string('a' + (rng+n)%rng))
+// }
+
+// // Vigenere cipher
+// type Vigenere struct {
+// 	key string
+// }
+// // NewVigenere returns a new cipher
+// func NewVigenere(key string) Cipher {
+// 	min := byte('a')
+// 	max := byte('a')
+// 	for i := 0; i < len(key); i++ {
+// 		if key[i] < min {
+// 			min = key[i]
+// 		}
+// 		if key[i] > max {
+// 			max = key[i]
+// 		}
+// 	}
+// 	if min < 'a' || max > 'z' || max == 'a' {
+// 		return nil
+// 	}
+// 	return &Vigenere{key}
+// }
+// // Encode cleans a message and encodes it using the Vigenere cipher
+// func (cipher *Vigenere) Encode(msg string) string {
+// 	var input bytes.Buffer
+// 	for _, c := range msg {
+// 		if c >= 'A' && c <= 'Z' {
+// 			input.WriteByte(byte(c) - 'A' + 'a')
+// 		} else if c >= 'a' && c <= 'z' {
+// 			input.WriteByte(byte(c))
+// 		}
+// 	}
+// 	clean := input.String()
+// 	var buffer bytes.Buffer
+// 	for i := 0; i < len(clean); i++ {
+// 		c := clean[i]
+// 		k := cipher.key[i%len(cipher.key)]
+// 		s := 'a' + ((c-'a')+(k-'a'))%rng
+// 		buffer.WriteByte(s)
+// 	}
+// 	return buffer.String()
+// }
+// // Decode decodes msg
+// func (cipher *Vigenere) Decode(msg string) string {
+// 	var buffer bytes.Buffer
+// 	for i := 0; i < len(msg); i++ {
+// 		c := msg[i]
+// 		k := cipher.key[i%len(cipher.key)]
+// 		s := 'a' + ((c-'a')-(k-'a')+rng)%rng
+// 		buffer.WriteByte(s)
+// 	}
+// 	return buffer.String()
+// }
