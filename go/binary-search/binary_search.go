7d76154447a59ca7ceab2bd9b61038a22dfeb7b9
fenogentov <ShuraF@andex.ru>
2021-09-29 20:22:27 +0300

    binary-search is completed

---
+++ go/binary-search/binary_search.go
@@ -0,0 +1,22 @@
+package binarysearch
+
+func SearchInts(sl []int, key int) int {
+	low := 0
+	high := len(sl) - 1
+
+	for low <= high {
+		median := (low + high) / 2
+
+		if sl[median] < key {
+			low = median + 1
+		} else {
+			high = median - 1
+		}
+	}
+
+	if low == len(sl) || sl[low] != key {
+		return -1
+	}
+
+	return low
+}
